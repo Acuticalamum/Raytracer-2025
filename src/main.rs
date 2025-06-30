19e0bc383a6dd583eac443bee0c8fdf5b5f3372f
Acuticalamum <acuticalamum007@sjtu.edu.cn>
2025-06-30 10:58:55 +0800

    final fix for main to generate img23.ppm

--- src/main.rs
+++ src/main.rs
@@ -30,64 +30,88 @@ use std::sync::Arc;
 use vec3::{Point3, Vec3};
 
 fn main() -> io::Result<()> {
-    let path = std::path::Path::new("output/book1/image22.ppm");
+    let path = std::path::Path::new("output/book1/image23.ppm");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
-    let file = File::create("output/book1/image22.ppm").expect("Failed to create file");
+    let file = File::create("output/book1/image23.ppm").expect("Failed to create file");
     let mut out = BufWriter::new(file);
-
-    let material_ground: Option<Arc<dyn Material>> =
-        Some(Arc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0))));
-    let material_center: Option<Arc<dyn Material>> =
-        Some(Arc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5))));
-    let material_left: Option<Arc<dyn Material>> = Some(Arc::new(Dielectric::new(1.50)));
-    let material_bubble: Option<Arc<dyn Material>> = Some(Arc::new(Dielectric::new(1.00 / 1.50)));
-    let material_right: Option<Arc<dyn Material>> =
-        Some(Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0)));
 
     let mut world = HittableList::new();
 
+    let ground_material: Option<Arc<dyn Material>> =
+        Some(Arc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5))));
     world.add(Arc::new(Sphere::new(
-        Point3::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
-    )));
-    world.add(Arc::new(Sphere::new(
-        Point3::new(0.0, 0.0, -1.2),
-        0.5,
-        material_center,
-    )));
-    world.add(Arc::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.5,
-        material_left,
-    )));
-    world.add(Arc::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.4,
-        material_bubble,
-    )));
-    world.add(Arc::new(Sphere::new(
-        Point3::new(1.0, 0.0, -1.0),
-        0.5,
-        material_right,
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
     )));
 
-    let mut cam = Camera::new(16.0 / 9.0, 400);
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rtweekend::random_double();
+            let center = Point3::new(
+                a as f64 + 0.9 * rtweekend::random_double(),
+                0.2,
+                b as f64 + 0.9 * rtweekend::random_double(),
+            );
 
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                let sphere_material: Option<Arc<dyn Material>>;
+
+                if choose_mat < 0.8 {
+                    let albedo = Color::random() * Color::random();
+                    sphere_material = Some(Arc::new(Lambertian::new(albedo)));
+                    world.add(Arc::new(Sphere::new(center, 0.2, sphere_material)));
+                } else if choose_mat < 0.95 {
+                    let albedo = Color::random_range(0.5, 1.0);
+                    let fuzz = rtweekend::random_double_range(0.0, 0.5);
+                    sphere_material = Some(Arc::new(Metal::new(albedo, fuzz)));
+                    world.add(Arc::new(Sphere::new(center, 0.2, sphere_material)));
+                } else {
+                    sphere_material = Some(Arc::new(Dielectric::new(1.5)));
+                    world.add(Arc::new(Sphere::new(center, 0.2, sphere_material)));
+                }
+            }
+        }
+    }
+
+    let material1: Option<Arc<dyn Material>> = Some(Arc::new(Dielectric::new(1.5)));
+    world.add(Arc::new(Sphere::new(
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
+    )));
+
+    let material2: Option<Arc<dyn Material>> =
+        Some(Arc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1))));
+    world.add(Arc::new(Sphere::new(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
+    )));
+
+    let material3: Option<Arc<dyn Material>> =
+        Some(Arc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0)));
+    world.add(Arc::new(Sphere::new(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
+    )));
+
+    let mut cam = Camera::new(16.0 / 9.0, 1200);
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400;
-    cam.samples_per_pixel = 100;
+    cam.image_width = 1200;
+    cam.samples_per_pixel = 500;
     cam.max_depth = 50;
 
     cam.vfov = 20.0;
-    cam.lookfrom = Point3::new(-2.0, 2.0, 1.0);
-    cam.lookat = Point3::new(0.0, 0.0, -1.0);
+    cam.lookfrom = Point3::new(13.0, 2.0, 3.0);
+    cam.lookat = Point3::new(0.0, 0.0, 0.0);
     cam.vup = Vec3::new(0.0, 1.0, 0.0);
 
-    cam.defocus_angle = 10.0;
-    cam.focus_dist = 3.4;
+    cam.defocus_angle = 0.6;
+    cam.focus_dist = 10.0;
 
     cam.initialize();
     cam.render(&world, &mut out)?;
